379692e4ef80e9e5d6ceffbeff998216f1f4c4f7
gogocrow <gogocrow@163.com>
2017-11-12 11:36:42 +0800

    add parser.

--- app/controller/hook.js
+++ app/controller/hook.js
@@ -1,9 +1,22 @@
 'use strict';
 
+async function publish(url, branch) {
+  
+}
+
 exports.push = async ctx => {
   const request = ctx.request.body;
 
   console.log(request);
 
-  
+  if (request.repository && request.repository.name) {
+    const config = ctx.app.config.projects[request.repository.name];
+    if (config) {
+      const branch = config.branch;
+      if (request.ref === `refs/heads/${branch}`) {
+
+      }
+    }
+  }
+
 };
